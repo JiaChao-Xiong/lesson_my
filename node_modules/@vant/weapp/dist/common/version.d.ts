13f48085e59cfdad00b37f85971ab8469b2b11af
JCX2927917567 <115977360+JCX2927917567@users.noreply.github.com>
2022-12-07 23:33:48 +0800

    Delete node_modules directory

--- node_modules/@vant/weapp/dist/common/version.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="miniprogram-api-typings" />
-export declare function getSystemInfoSync(): WechatMiniprogram.SystemInfo;
-export declare function canIUseModel(): boolean;
-export declare function canIUseFormFieldButton(): boolean;
-export declare function canIUseAnimate(): boolean;
-export declare function canIUseGroupSetData(): boolean;
-export declare function canIUseNextTick(): boolean;
-export declare function canIUseCanvas2d(): boolean;
-export declare function canIUseGetUserProfile(): boolean;
